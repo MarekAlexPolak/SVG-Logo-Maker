69ec21984fe2f8dcedb7c44f407fbf660433e2ca
Marek Polak <markalexpolak@gmail.com>
2023-05-15 16:50:40 -0400

    cleaned up test file and added a generate.js file to generate the logo

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const {Square, Triangle, Circle} = require("./lib/shapes");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {Square, Triangle, Circle} = require('./lib/shapes');
+const generateSVG = require('./lib/generate');
+
 
 const questions = [
     {
-        type: "input",
-        name: "text",
-        message:"Please enter what you would like your logo to display? (This must be between 0 and 3 characters)"
+        type: 'input',
+        name: 'text',
+        message:'Please enter what you would like your logo to display? (This must be between 0 and 3 characters)',
+        validate: (input) => 0 < input.length < 4
     },
     {
-        type: "input",
-        name: "colour",
-        message:"Please choose a text colour by typing the name or hexidecimal code."
+        type: 'input',
+        name: 'colour',
+        message:'Please choose a text colour by typing the name or hexidecimal code.'
     },
     {
-        type: "input",
-        name: "shapeColour",
-        message:"Please input your desired logo colour by typing the name or hecidecimal code."
+        type: 'input',
+        name: 'shapeColour',
+        message:'Please input your desired logo colour by typing the name or hecidecimal code.'
     },
     {
-        type: "list",
-        name: "shape",
-        message:"Please choose your desired shape.",
-        choices: ["Square", "Triangle", "Circle"]
+        type: 'list',
+        name: 'shape',
+        message:'Please choose your desired shape.',
+        choices: ['Square', 'Triangle', 'Circle']
     }
 ]
 
-function generate(){
-
-}
 
 function askQuest() {
-    inquirer.prompt(questions).then((data) => {
-        generate(data);
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeFile('logo.svg', generateSVG);
     })
 }
 
